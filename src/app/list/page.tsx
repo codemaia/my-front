f9fc604afd7cb00770c90ce5e67e1dbbddb5edde
codemaia <renanmaia.rl@gmail.com>
2024-05-06 22:42:51 -0300

    add layout in the list page and method delete book

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,41 +1,76 @@
 'use client'
 
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 
-export default function List() {
+export default function ListBook() {
 
     const [dataBook, setDataBook] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        const fetchData = async (type: any): Promise<void> => {
-            try {
-                const response = await fetch('http://localhost:8080/books');
-
-                if (!response.ok) {
-                    throw new Error(':( Error accessing data')
-                }
-
-                const data = await response.json();
-                setDataBook(data);
-
-            } catch (error) {
-                console.log(error);
-            }
-        };
-
-        fetchData('someType');
-
+        fetchData();
     }, []);
+        
+    const fetchData = async (): Promise<void> => {
+        try {
+            const response = await fetch('http://localhost:8080/books');
 
+            if (!response.ok) {
+                throw new Error(':( Error accessing data')
+            }
+
+            const data = await response.json();
+            setDataBook(data);
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleDelete = async (id: string): Promise<void> => {    
+        try {
+            setLoading(true);
+            const response = await fetch(`http://localhost:8080/books/${id}`, {
+                method: 'DELETE',
+            });
+            if (response.ok) {
+                alert('Livro excluido com sucesso!');
+                fetchData();
+            } else { 
+                alert('Erro ao excluir livro');
+                console.error('Erro ao excluir livro', response.status);
+            }
+        } catch(error) {
+            console.error('Erro ao excluir livro: ', error);
+        } finally {
+            setLoading(false);
+        }
+    
+    }
 
     return (
-        <div>
-            <h1>Aqui vou listar</h1>
-            <ul>
+        
+        <div className="container mx-auto px-4 py-8">
+            <h1 className="text-2xl font-bold mb-4">Lista de Livros</h1>
+            {loading ? (
+                <p className="text-gray-500">Carregando...</p>
+            ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {dataBook.map((book: any) => (
-                    <li key={book.id}>Livro: {book.name} Autor: {book.author}</li>
+                    <div key={book.id} className="border rounded-md p-4">
+                        <h2 className="text-xl font-semibold mb-2">{book.name}</h2>
+                        <p className="text-gray-600">{book.author}</p>
+                        <p className="text-gray-600">{book.editora}</p>
+                        <div className="mt-4 flex justify-between">
+                                <a className="text-indigo-500 hover:text-indigo-700">Editar</a>
+                            <button className="text-red-600 hover:text-red-800" onClick={() => handleDelete(book.id)}>Excluir</button>
+                        </div>
+                    </div>
                 ))}
-            </ul>
+            </div>
+            )}
         </div>
+
     )
 }
